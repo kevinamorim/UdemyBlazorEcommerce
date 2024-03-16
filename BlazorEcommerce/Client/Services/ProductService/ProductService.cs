32d60bf483a50b8eee1c7ece0595fcfe4d1f0227
Kevin Amorim <kevinamorim4@gmail.com>
2024-03-16 16:44:53 +0000

    Search component.

--- BlazorEcommerce/Client/Services/ProductService/ProductService.cs
+++ BlazorEcommerce/Client/Services/ProductService/ProductService.cs
@@ -11,6 +11,7 @@ namespace BlazorEcommerce.Client.Services.ProductService
         }
 
         public List<Product> Products { get; set; } = new List<Product>();
+        public string Message { get; set; } = "Loading products...";
 
         public event Action ProductsChanged;
 
@@ -27,6 +28,24 @@ namespace BlazorEcommerce.Client.Services.ProductService
                 await _http.GetFromJsonAsync<ServiceResponse<List<Product>>>($"api/product/category/{categoryUrl}");
             if (result != null && result.Data != null)
                 Products = result.Data;
+
+            ProductsChanged.Invoke();
+        }
+
+        public async Task<List<string>> GetProductSearchSuggestion(string searchText)
+        {
+            var result = await _http.GetFromJsonAsync<ServiceResponse<List<string>>>($"api/product/searchSuggestions/{searchText}");
+            return result?.Data ?? new List<string>();
+        }
+
+        public async Task SearchProducts(string searchText)
+        {
+            var result = await _http.GetFromJsonAsync<ServiceResponse<List<Product>>>($"api/product/search/{searchText}");
+            if (result != null && result.Data != null)
+                Products = result.Data;
+
+            if (Products.Count == 0)
+                Message = "No products found.";
 
             ProductsChanged.Invoke();
         }
